4ef47f00c7f9abb01f2fc0d6d0abae10844a5c9d
deepakmhnp25 <deepakmhnp25@gmail.com>
2022-09-26 20:03:52 +0200

    calculator challenge

---
+++ challenges/calculator.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+This will sum two values
+exception for non numbers
+user should be able to select mathematical operation
+user should be able to see how long it took for the operation to complete
+*/
+func main() {
+	fmt.Println("Hello")
+	panic("Application is exiting ...")
+}
